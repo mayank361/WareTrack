fd6e846c690b9507109688855c7c0da35f9cf616
Mayank_Singh <singhmayank361@gmail.com>
2024-02-23 16:41:31 +0530

    Tokens added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,10 @@
 const  asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const jwt= require("jsonwebtoken")
+
+const generateToken =(id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: "1d"})
+}
 
 const registerUser= asyncHandler(async (req, res) => {
     const {name,email,password } = req.body
@@ -22,16 +27,22 @@ const registerUser= asyncHandler(async (req, res) => {
         throw new Error("Email has already been registered")
     }
 
+    
+
     // Create new user
     const user =await User.create({
         name,
         email,
         password
-    })
+    });
+
+    // Generate Token
+const token= generateToken(user._id)
+
     if (user){
         const {_id, name, email, photo, phone, bio} = user
         res.status(201).json({
-            _id,name, email, photo, phone, bio
+            _id,name, email, photo, phone, bio,token,
         })
     } else{
         res.status(400)
@@ -39,6 +50,7 @@ const registerUser= asyncHandler(async (req, res) => {
     }
 
 });
+
 
 module.exports ={
     registerUser,
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
+
 
 const userSchema = mongoose.Schema({
 name:{
@@ -19,7 +21,7 @@ password:{
     type: String,
     required: [true, "Please add a password"],
     minLength: [6,"Password must be up to 6 characters"],
-    maxLength: [23,"Password must not be more than 23 characters"],
+    //maxLength: [23,"Password must not be more than 23 characters"],
 },
 photo:{
     type: String,
@@ -39,6 +41,19 @@ bio:{
     timestamps: true,
 }
 );
+
+//Encrypt password before saving to DB
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")){
+        return next()
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(this.password, salt)
+    this.password = hashedPassword
+    next();
+})
 
 const User = mongoose.model("User", userSchema)
 module.exports= User
